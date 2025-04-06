f6a0569d9ccc6e55f342f1a5b42fc354176f240b
Aestu <aryapangestu2024@gmail.com>
2025-04-06 22:41:49 +0700

    Update lkp1.ino

--- Belajar/lkp1.ino
+++ Belajar/lkp1.ino
@@ -1,29 +1,20 @@
-int LED1 = 16;  // D0
-int LED2 = 5;   // D1
-int LED3 = 4;   // D2
-int LED4 = 0;   // D3
-int LED5 = 2;   // D4
+// Deklarasi array yang berisi semua pin LED
+int leds[] = {16, 5, 4, 0, 2}; // D0, D1, D2, D3, D4
+int jumlahLED = 5; // Jumlah LED
 
 void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
+  // Mengatur semua pin LED sebagai OUTPUT
+  for (int i = 0; i < jumlahLED; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
 }
 
 void loop() {
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED3, HIGH);
-  digitalWrite(LED4, HIGH);
-  digitalWrite(LED5, HIGH);
-  delay(1000);
-
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
-  digitalWrite(LED3, LOW);
-  digitalWrite(LED4, LOW);
-  digitalWrite(LED5, LOW);
-  delay(1000);
+  // Menyalakan dan mematikan LED satu per satu
+  for (int i = 0; i < jumlahLED; i++) {
+    digitalWrite(leds[i], HIGH); // Nyalakan LED ke-i
+    delay(1000);                 // Tunggu 1 detik
+    digitalWrite(leds[i], LOW);  // Matikan LED ke-i
+    delay(1000);                 // Tunggu 1 detik
+  }
 }
